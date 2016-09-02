dece37da844f0f85c7c2310c8c35c8d3b690f6ef
ariy-parallel <yes6sss1@gmail.com>
2016-09-02 18:53:09 +0900

    Hole追加

---
+++ Assets/Script/Hole.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hole : MonoBehaviour {
+
+    public string activeTag;
+    public float force;
+    public bool fallIn { get; private set; }
+
+    private bool isActivetag(Collider other)
+    {
+        return other.gameObject.tag == activeTag;
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (isActivetag(other))
+        {
+            fallIn = true;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (isActivetag(other))
+        {
+            fallIn = false;
+        }
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+        var direction = transform.position - other.gameObject.transform.position;
+        direction.Normalize();
+
+        var r = other.gameObject.GetComponent<Rigidbody> ();
+        if (isActivetag(other))
+        {
+            r.velocity *= 0.9f;
+            r.AddForce (direction * r.mass * force);
+        }
+        else
+        {
+            r.AddForce (- direction * r.mass * 80.0f);
+        }
+    }
+}
---
+++ Assets/Script/Holes.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Holes : MonoBehaviour {
+
+    public Hole red;
+    public Hole blue;
+    public Hole green;
+
+    private Rect labelPosition = new Rect (0, 0, 100, 30);
+
+    private string getLabel()
+    {
+        if (red.fallIn && blue.fallIn && green.fallIn)
+        {
+            return "Fall in hole!";
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label (labelPosition, getLabel ());
+    }
+}
